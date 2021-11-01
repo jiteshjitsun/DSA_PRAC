387056f5ef24203ab9a68e4ee3c33769c010545b
jitesh singh <jiteshjitsun@gmail.com>
2021-11-01 22:57:58 +0530

    hehe :)

---
+++ NODE/Linked List Cycle.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *temp = head;
+        unordered_set<ListNode*> mp;
+        
+        while(temp!=NULL) {
+            if(mp.find(temp) != mp.end()) return true;
+            mp.insert(temp);
+            temp = temp->next;
+        }
+        
+        return false;
+    }
+};
